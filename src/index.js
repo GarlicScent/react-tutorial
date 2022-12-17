e4d2a483e3e92f684e6740b835f91ca58444c979
GarlicScent <dongzai@naver.com>
2022-12-17 13:54:01 +0900

    사용자와 상효작용하는 컴포넌트 만들기. state 사용

--- src/index.js
+++ src/index.js
@@ -3,15 +3,22 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 class Square extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			value: null,
+		};
+	}
+
 	render() {
 		return (
 			<button
 				className="square"
 				onClick={() => {
-					console.log("click");
+					this.setState({ value: "X" });
 				}}
 			>
-				{this.props.value}
+				{this.state.value}
 			</button>
 		);
 	}
