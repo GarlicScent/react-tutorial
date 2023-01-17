b599bc0175ab54609772944faae3056a86e570ba
GarlicScent <dongzai@naver.com>
2023-01-17 17:27:58 +0900

    #7.0~1 To Do List with React

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
-import Button from "./Button";
-import styles from "./App.module.css";
-import { useState, useEffect } from "react";
-import { render } from "@testing-library/react";
-
-function Hello() {
-	function byeFn() {
-		console.log("bye");
-	}
-	function hiFn() {
-		console.log("created :)");
-		return byeFn;
-	}
-	useEffect(hiFn, []);
-	return <h1>Hello</h1>;
-}
+import { useState } from "react";
 
 function App() {
-	const [showing, setShowing] = useState(false);
-
-	function revertClick() {
-		setShowing((prev) => !prev);
-	}
+	const [todo, setTodo] = useState("");
+	const [todos, setTodos] = useState([]);
+	const onChange = (event) => setTodo(event.target.value);
+	const onSubmit = (event) => {
+		event.preventDefault();
+		if (todo === "") {
+			return;
+		}
+		setTodo("");
+		setTodos((currentArray) => [todo, ...currentArray]);
+	};
+	console.log(todo, todos);
 	return (
 		<div>
-			{showing ? <Hello /> : null}
-			<button onClick={revertClick}>{showing ? "Hide" : "Show"}</button>
+			<h1>My To Dos</h1>
+			<h4>(left todos: {todos.length})</h4>
+			<form onSubmit={onSubmit}>
+				<input
+					value={todo}
+					onChange={onChange}
+					type="text"
+					placeholder="Write your to do..."
+				/>
+				<button type="submit">Add To Do</button>
+			</form>
+			<hr />
+			<ul>
+				{todos.map((todo, index) => (
+					<li key={index}>{todo.toUpperCase()}</li>
+				))}
+			</ul>
 		</div>
 	);
 }
+
 export default App;
