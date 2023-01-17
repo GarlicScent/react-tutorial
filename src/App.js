7552f245ea59b640b69f315cf0a9ea89f794dc68
GarlicScent <dongzai@naver.com>
2023-01-17 13:06:38 +0900

    #6.0~6.3 useEffect. and solve warning console about reactDOM

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+	const [counter, setCounter] = useState(0);
+	const [keyword, setKeyword] = useState("");
+	const onClick = () => setCounter((prev) => prev + 1);
+	const onChange = (event) => setKeyword(event.target.value);
+
+	useEffect(() => console.log("call the counter"), [counter]);
+	useEffect(() => {
+		if (keyword !== "" && keyword.length > 4)
+			console.log("Search for: ", keyword);
+	}, [keyword]);
 	return (
 		<div>
-			<h1 className={styles.title}>Welcome back!</h1>
-			<Button text={"This Button"} />
+			<input
+				onChange={onChange}
+				type="text"
+				placeholder="Search here..."
+			/>
+
+			<h1 className={styles.title}>{counter}</h1>
+			<button onClick={onClick}>Click me</button>
 		</div>
 	);
 }
